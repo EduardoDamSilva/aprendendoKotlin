975b3e95fe25486d00faff2a5da32ec4b96b6a8d
EduardoDamSilva <eduardo.silva@sptech.school>
2022-12-22 18:48:01 -0300

    while sintaxe

---
+++ controleDeFluxo/src/main/kotlin/while.kt
@@ -0,0 +1,15 @@
+fun main (){
+
+    var i = 0
+    while (i < 100) {
+        print("$i ")
+        i++
+    }
+
+    var s = "Kotlin"
+    while (i < s.length){
+        print("${s[i]}")
+        i++
+    }
+}
+
