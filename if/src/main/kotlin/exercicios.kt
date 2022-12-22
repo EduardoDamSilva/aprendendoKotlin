1a7fef8029a4d45e84b3bd3fe8ca22f8daa434df
EduardoDamSilva <eduardo.silva@sptech.school>
2022-12-22 15:14:04 -0300

    resolução de mais exercicio usando if

--- if/src/main/kotlin/exercicios.kt
+++ if/src/main/kotlin/exercicios.kt
@@ -30,6 +30,46 @@ fun bonus(cargo: String, experienciaAnos: Int) : Int{
     }
 
 }
+/*
+    3 - Escreva um programa capaz de ler dois números inteiros que representam os lados de uma figura
+    geométrica. Informar se formam um quadrado (lados iguais).
+*/
+
+fun verificaQuadrado(n1: Int, n2: Int): String{
+    if (n1 == n2 && n1 > 0 && n2 > 0){
+        return "É um quadrado"
+    } else {
+        return "Não é um quadrado"
+    }
+}
+
+/*
+    4 - Escreva um programa capaz de ler três números inteiros que representam os lados de um triângulo. Informar
+    se é um triângulo equilátero (todos os lados iguais)
+*/
+
+fun verificarTriangulo(n1: Int, n2: Int, n3: Int): String{
+    if ((n1 == n2 && n2 == n3)  && (n1 > 0 && n2 > 0 && n3 > 0)){
+        return "É um triângulo"
+    } else {
+        return "Não é um triângulo"
+    }
+}
+
+/*
+    5 - Considere o código abaixo. Qual a saída do programa caso informado o valor 4?
+*/
+fun qualASaida(num: Int) {
+    if (num >= 0) {
+        if (num == 0)
+            println("Primeira string")
+        else println("Segunda string")
+    }
+    println("Terceira string")
+}
+// Saída:
+// Segunda String
+// Terceira String
 
 fun main (){
     println(bonus("Gerente", 2))
