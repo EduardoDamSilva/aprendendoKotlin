ad6d41f15a3440f2bd4456fec5a1cd06a672fb72
EduardoDamSilva <eduardo.silva@sptech.school>
2023-01-10 17:42:28 -0300

    doWhile aprendendo a sintax

---
+++ controleDeFluxo/src/main/kotlin/doWhile.kt
@@ -0,0 +1,16 @@
+fun main (){
+
+//  Do while: executa o código uma vez e depois testa a condição que está no while
+//    var i = 0
+//    do {
+//        print("$i ")
+//        i++
+//    } while (i == 1000)
+
+    do {
+        print("Qual seu nome: ")
+        val value = readLine()
+    } while (value == "")
+
+
+}
