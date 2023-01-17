0a839fe64020cdb4a552eaa00c3ce2112f09e15f
EduardoDamSilva <eduardo.silva@sptech.school>
2023-01-17 15:13:18 -0300

    aprendendo sobre mais sobre parametros, como predifinição de um valor

---
+++ maisFuncoes/src/main/kotlin/parametros.kt
@@ -0,0 +1,7 @@
+fun main() {
+    endereco(cidade = "cidade", rua = "street", estado = "widaw") // Não é necessário informar o cep e o num por que eles já tem um valor padrão na própria função
+}
+
+fun endereco(rua: String, cidade: String, estado: String, cep: String = "", num: Int = 0) {
+
+}
